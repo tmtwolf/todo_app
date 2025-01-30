97b54f84b0144e7d5deb1571238f72e32b140012
tmtwolf <tmtwolf.main@gmail.com>
2025-01-30 21:21:04 +0900

    -- little fix

--- api/index.go
+++ api/index.go
@@ -2,9 +2,306 @@ package api
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+	"text/template"
+	"todo_app/app/models"
+	"todo_app/config"
 )
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello")
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, filename := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", filename))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
+
+// セッションがあればセッションを返し、なければエラーを返す
+func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err error) {
+	cookie, err := r.Cookie("_cookie")
+	if err == nil {
+		sess := models.Session{UUID: cookie.Value}
+		if ok, _ := sess.CheckSession(); !ok {
+			err = fmt.Errorf("Invalid session")
+		}
+		return sess, err
+	}
+	return
+}
+
+// URLの正規表現の型
+var validPath = regexp.MustCompile("^/todos/(edit|update|delete)/([0-9]+)$")
+
+func parseURL(fn func(http.ResponseWriter, *http.Request, int)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// _/todos/edit/1
+		q := validPath.FindStringSubmatch(r.URL.Path)
+		if q == nil {
+			fmt.Println("-- ID is NotFound")
+			http.NotFound(w, r)
+			return
+		}
+
+		qi, err := strconv.Atoi(q[2]) // q[2]、インデックス番号2はIDとして受け取る
+		if err != nil {
+			fmt.Println("-- ID is NotMatch")
+			http.NotFound(w, r)
+			return
+		}
+
+		fmt.Println("-- ID is Match: ", qi)
+
+		fn(w, r, qi)
+	}
+}
+
+func StartMainServer() (err error) {
+	files := http.FileServer(http.Dir(config.Config.Static))
+	http.Handle("/static/", http.StripPrefix("/static/", files))
+
+	http.HandleFunc("/", top)
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/authenticate", authenticate)
+	http.HandleFunc("/todos", index)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/todos/new", todoNew)
+	http.HandleFunc("/todos/save", todoSave)
+	http.HandleFunc("/todos/edit/", parseURL(todoEdit))
+	http.HandleFunc("/todos/update/", parseURL(todoUpdate))
+	http.HandleFunc("/todos/delete/", parseURL(todoDelete))
+
+	port := os.Getenv("PORT")
+	return http.ListenAndServe(":"+port, nil)
+}
+
+// ! Route main
+
+func top(w http.ResponseWriter, r *http.Request) {
+	_, err := session(w, r)
+	if err != nil {
+		generateHTML(w, "Hello", "layout", "public_navbar", "top")
+	} else {
+		http.Redirect(w, r, "/todos", 302)
+	}
+
+	/*
+		t, err := template.ParseFiles("app/views/templates/top.html")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		t.Execute(w, "hello")
+	*/
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/", 302)
+	} else {
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		todos, _ := user.GetTodosByUser()
+		user.Todos = todos
+		generateHTML(w, user, "layout", "private_navbar", "index")
+	}
+}
+
+func todoNew(w http.ResponseWriter, r *http.Request) {
+	_, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		generateHTML(w, nil, "layout", "private_navbar", "todo_new")
+	}
+}
+
+func todoSave(w http.ResponseWriter, r *http.Request) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else if r.Method == "POST" {
+		// フォームの値の取得、パースフォーム
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+
+		// 保存先のユーザーの取得
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		// _Todoの作成
+		err = user.CreateTodo(r.PostFormValue("content"))
+		if err != nil {
+			log.Panicln(err)
+		}
+		http.Redirect(w, r, "/index", 302)
+	}
+}
+
+func todoEdit(w http.ResponseWriter, r *http.Request, id int) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		_, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		t, err := models.GetTodoById(id)
+		if err != nil {
+			log.Println(err)
+		}
+		generateHTML(w, t, "layout", "private_navbar", "todo_edit")
+	}
+}
+
+func todoUpdate(w http.ResponseWriter, r *http.Request, id int) {
+	_, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else if r.Method == "POST" {
+		// フォームの値の取得、パースフォーム
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+
+		// アップデート先Todoの取得
+		todo, err := models.GetTodoById(id)
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		// _Todoの更新
+		todo.Content = r.PostFormValue("content")
+		err = todo.UpdateTodo()
+		if err != nil {
+			log.Panicln(err)
+		}
+		http.Redirect(w, r, "/index", 302)
+	}
+}
+
+func todoDelete(w http.ResponseWriter, r *http.Request, id int) {
+	_, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		// フォームの値の取得、パースフォーム
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+
+		// 削除先Todoの取得
+		todo, err := models.GetTodoById(id)
+		if err != nil {
+			log.Println(err)
+		}
+
+		// _Todoの削除
+		err = todo.DeleteTodo()
+		if err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/index", 302)
+	}
+}
+
+// ! Route Auth
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		_, err := session(w, r)
+		if err != nil {
+			generateHTML(w, nil, "layout", "public_navbar", "signup")
+		} else {
+			http.Redirect(w, r, "/todos", 302)
+		}
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		user := models.User{
+			Name:     r.PostFormValue("name"),
+			Email:    r.PostFormValue("email"),
+			Password: r.PostFormValue("password"),
+		}
+
+		if err := user.CreateUser(); err != nil {
+			log.Fatalln(err)
+		}
+
+		http.Redirect(w, r, "/", 302)
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	_, err := session(w, r)
+	if err != nil {
+		generateHTML(w, nil, "layout", "public_navbar", "login")
+	} else {
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
+
+func authenticate(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	user, err := models.GetUserByEmail(r.PostFormValue("email"))
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	}
+
+	if user.Password == models.Encrypt(r.PostFormValue("password")) {
+		sess, err := user.CreateSession()
+		if err != nil {
+			log.Println(err)
+		}
+
+		cookie := http.Cookie{
+			Name:     "_cookie",
+			Value:    sess.UUID,
+			HttpOnly: true,
+		}
+		http.SetCookie(w, &cookie)
+
+		http.Redirect(w, r, "/top", 302)
+	} else {
+		http.Redirect(w, r, "/login", 302)
+	}
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("_cookie")
+	if err != nil {
+		log.Println(err)
+	}
+
+	if err != http.ErrNoCookie {
+		sess := models.Session{
+			UUID: cookie.Value,
+		}
+
+		if err = sess.DeleteSessionByUUID(); err != nil {
+			log.Println("セッションの削除に失敗")
+		}
+
+		http.Redirect(w, r, "/login", 302)
+	}
 }
