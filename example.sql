4ff2b843312b2c106aae114210746557a16712bd
tmtwolf <tmtwolf.main@gmail.com>
2025-01-30 17:54:14 +0900

    update for vercel

---
+++ example.sql
@@ -0,0 +1,23 @@
+create table users (
+    id         serial primary key,
+    uuid       varchar(64) not null unique,
+    name       varchar(255),
+    email      varchar(255) not null unique,
+    password   varchar(255) not null,
+    created_at timestamp not null   
+);
+
+create table sessions (
+    id         serial primary key,
+    uuid       varchar(64) not null unique,
+    email      varchar(255),
+    user_id    integer references users(id),
+    created_at timestamp not null   
+);
+
+create table todos (
+    id         serial primary key,
+    content    text,
+    user_id    integer references users(id),
+    created_at timestamp not null       
+);
